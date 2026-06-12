65102fcc1b0487056e5c84137d31adc621e68ae3
Yassa Talaat <145781920+YassaTalaat80@users.noreply.github.com>
2026-06-12 18:20:16 +0300

    feat(validation): add validation rules to User request DTOs

--- src/main/java/com/dev/ShopCart/request/UserUpdateRequest.java
+++ src/main/java/com/dev/ShopCart/request/UserUpdateRequest.java
@@ -1,9 +1,13 @@
 package com.dev.ShopCart.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class UserUpdateRequest {
+    @NotBlank(message = "First name is required")
     private String firstName;
+
+    @NotBlank(message = "Last name is required")
     private String lastName;
 }
