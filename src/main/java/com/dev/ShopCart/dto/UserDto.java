658cdd6f41f447663dd4c230fc10fd301e2f79fa
Yassa Talaat <145781920+YassaTalaat80@users.noreply.github.com>
2026-06-12 18:22:23 +0300

    feat(user): update UserRepository and add roles to UserDto

--- src/main/java/com/dev/ShopCart/dto/UserDto.java
+++ src/main/java/com/dev/ShopCart/dto/UserDto.java
@@ -1,9 +1,11 @@
 package com.dev.ShopCart.dto;
+import java.util.Set;
 
 public record UserDto(
         Long id,
         String firstName,
         String lastName,
         String email,
-        CartDto cart
+        CartDto cart,
+        Set<String> roles
 ) {}
--- src/main/java/com/dev/ShopCart/repository/UserRepository.java
+++ src/main/java/com/dev/ShopCart/repository/UserRepository.java
@@ -3,6 +3,11 @@ package com.dev.ShopCart.repository;
 import com.dev.ShopCart.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+import java.util.OptionalLong;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
